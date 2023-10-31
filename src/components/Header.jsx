840adcd242e7d6947e84a5502a619d555ef6a656
Lima <sn.scac1@gmail.com>
2023-10-31 14:23:53 +0200

    Header up, middle, down
    Scrolling nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-import './header.css';
-import { Link, NavLink } from "react-router-dom"
-import { BiMenu } from 'react-icons/bi'
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import './header.scss';
+import { Link, NavLink } from "react-router-dom";
+import { BiMenu, BiUserCircle } from 'react-icons/bi';
+import { AiOutlineCloseCircle, AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import { TfiHeadphoneAlt } from 'react-icons/tfi';
+import { FiTablet } from 'react-icons/fi';
+import { IoWatchOutline } from 'react-icons/io5';
+import { BsPhone, BsLaptop, BsHeadphones } from 'react-icons/bs';
+
 
 
 export default function Header() {
@@ -10,7 +15,7 @@ export default function Header() {
 
     const activeStyles = {
         fontWeight: "bold",
-        color: "#C7F9CC",
+        // color: "#C7F9CC",
     }
 
     const closeBurgerMenu = () => {
@@ -20,105 +25,117 @@ export default function Header() {
     return (
         <header>
 
-            <section className='header-container' >
-                <Link className="site-logo" to="/" onClick={closeBurgerMenu}>
-                    <h2>Electro</h2>
-                </Link>
-                    <div className='burger-menu-button' onClick={() => setIsBurgerMenuOpen(!isBurgerMenuOpen)}>
-                        <BiMenu style={{ width: '1.7rem', height: '1.7rem' }} />
-                    </div>
-            </section>
+            <div className='header-up' >
+                <NavLink to="/Call">
+                    <TfiHeadphoneAlt />
+                    <span>078 000 111</span>
+                </NavLink>
+            </div>
 
-
-
-            <nav className={`burger-container ${isBurgerMenuOpen ? 'open' : ''}`}>
-                        <div className='burger-container-close-button' onClick={closeBurgerMenu}>
-                            <AiOutlineCloseCircle style={{ width: '1.7rem', height: '1.7rem' }} />
-                        </div>
-
-                    <div className="burger-container-inner">
-                        <div className="account-row">
-                                <NavLink to="/magazine" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Magazine
-                                </NavLink>
-                                <NavLink to="/call" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Sună
-                                </NavLink>
-                        </div>
-                        
-                        <ul className='nav-list'>
-                            <li>
-                                <NavLink to="/conectare" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Conectare
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/telefoane" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Telefoane
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/accesorii" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Accesorii
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/exclusiv" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Exclusiv
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/Apple" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Apple
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/Tablete" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Tablete
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/Laptopuri" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Laptopuri
-                                 </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/Gadgeturi" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Gadgeturi
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/Tradein" 
-                                    style={({ isActive }) => isActive ? activeStyles : null}
-                                    onClick={closeBurgerMenu}>
-                                    Tradein
-                                </NavLink>
-                            </li>
-                        </ul>
-                   
-
+            <div className='header-middle'>
+                <div className="header-middle-site-logo">
+                    <Link  to="/" 
+                        onClick={closeBurgerMenu}>
+                        <h2>Electro</h2>
+                    </Link>
                 </div>
-            </nav>
+                <div className="header-middle-icons">
+                    <NavLink>
+                        <AiOutlineSearch className="header-icon" />
+                    </NavLink>  
+                    <NavLink>
+                        <AiOutlineShoppingCart className="header-icon"/> 
+                    </NavLink>  
+                    <NavLink>
+                        <BiUserCircle className="header-icon"/>
+                    </NavLink>  
+                    <div 
+                        className='burger-menu-button' 
+                        onClick={() => setIsBurgerMenuOpen(!isBurgerMenuOpen)}>
+                        <BiMenu className="header-icon header-icon-burger"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='header-down'>
+                <nav className="horizontal-scroll">
+                    <ul>
+                        <li>
+                            <NavLink to="/Telefoane" 
+                                style={({ isActive }) => isActive ? activeStyles : null}
+                                onClick={closeBurgerMenu}>
+                                <p>Telefoane</p>
+                                <BsPhone />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Laptopuri" 
+                                style={({ isActive }) => isActive ? activeStyles : null}
+                                onClick={closeBurgerMenu}>
+                                <p>Laptopuri</p>
+                                <BsLaptop />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Gadgeturi" 
+                                style={({ isActive }) => isActive ? activeStyles : null}
+                                onClick={closeBurgerMenu}>
+                                <p>Gadgeturi</p>
+                                <IoWatchOutline />
+                            </NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/Tablete" 
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                            onClick={closeBurgerMenu}>
+                            <p>Tablete</p>
+                            <FiTablet />
+                        </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Accesorii" 
+                                style={({ isActive }) => isActive ? activeStyles : null}
+                                onClick={closeBurgerMenu}>
+                                <p>Accesorii</p>
+                                <BsHeadphones />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+
+            <div className={`burger-container ${isBurgerMenuOpen ? 'open' : ''}`}>
+                <div className='burger-container-close-button' onClick={closeBurgerMenu}>
+                    <AiOutlineCloseCircle style={{ width: '1.7rem', height: '1.7rem' }} />
+                </div>
+                <NavLink to="/Magazine" 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                    onClick={closeBurgerMenu}>
+                    Magazine
+                </NavLink>
+                <NavLink to="/Call" 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                    onClick={closeBurgerMenu}>
+                    Sună
+                </NavLink>
+                <NavLink to="/Exclusiv" 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                    onClick={closeBurgerMenu}>
+                    Exclusiv
+                </NavLink>
+                <NavLink to="/Conectare" 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                    onClick={closeBurgerMenu}>
+                    Conectare
+                </NavLink>
+                <NavLink to="/Tradein" 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                    onClick={closeBurgerMenu}>
+                    Tradein
+                </NavLink>
+            </div> 
+          
         </header>
     )
 }
