566912fb0947b8a441d10d56fa3cd20a01711970
Lima <sn.scac1@gmail.com>
2023-11-10 23:30:58 +0200

    Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
